3c74adfadfb903dd66b629b89d5fe6355e19d4c2
Alexander <antidze.sasha@gmail.com>
2021-10-29 16:48:23 +0400

    base calendar ui

--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -26,6 +26,72 @@
         <div class="mb-6 {{!$this->selectedService || !$this->selectedEmployee ? 'opacity-25' : ''}}">
             <label for="employee" class="inline-block text-gray-700 font-bold mb-2">Pick Appointment time</label>
         </div>
+
+
+
+
+
+
+        <div class="bg-white rounded-lg">
+
+            <div class="flex items-center justify-center relative">
+
+                <button type="button" class="p-4 absolute left-0 top-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 hover:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+                    </svg>
+                </button>   
+
+
+
+                <div class="text-lg font-semibold p-4">
+                    Jan 2021
+                </div>
+
+
+                <button type="button" class="p-4 absolute right-0 top-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 hover:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                    </svg>
+                </button>
+
+
+
+
+
+            </div>
+
+
+            <div class="flex justify-between items-center px-3 border-b border-gray-200 pb-2">
+                <button type="button" class="text-center group cursor-pointer focus:outline-none">
+
+                    <div class="text-xs leading-none mb-2 text-gray-700">Mon</div>
+
+                    <div class="text-lg leading-none p-1 rounded-full w-9 h-9 flex items-center justify-center group-hover:bg-gray-200">1</div>
+
+                </button>
+            </div>
+
+
+            <div class="max-h-52 overflow-y-scroll">
+                <input type="radio" name="time" id="" value="" class="sr-only">
+
+                <label for="" class="w-full text-left focus:outline-none px-4 py-2 flex items-center cursor-pointer border-b border-gray-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                    </svg>
+                    <span>9:00am</span>    
+                </label>
+
+                <div class="text-center text-gray-700 px-4 py-2">
+                    No available slots
+                </div>
+
+            </div>
+
+
+            
+        </div>
         
         
     </form>
