1dc90e497ed44f42e3a5131df4b1b6ad30abda90
Alexander <antidze.sasha@gmail.com>
2021-11-03 19:32:59 +0400

    adding calendar for new schedule, checking for existing schedules

--- app/Http/Livewire/CreateSchedule.php
+++ app/Http/Livewire/CreateSchedule.php
@@ -3,23 +3,46 @@
 namespace App\Http\Livewire;
 
 use App\Models\Employee;
+use App\Models\Schedule;
 use App\Models\Service;
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
 use Livewire\Component;
 
 class CreateSchedule extends Component
 {
 
+    public const INCREMENT = 15;
+
+    public $date;
     public $services;
+    public $calendarStartDate;
+    public $defaultStartTime;
+    public $defaultEndTime;
+    public $timeSlot;
 
     public $state = [
         'employee' => null,
         'service' => null,
+        'start_time' => null,
     ];
 
 
     public function mount()
     {
+        $this->setDate(now()->timestamp);
+        $this->calendarStartDate = now();
         $this->services = collect();
+
+        $this->defaultStartTime = '08:00';
+        $this->defaultEndTime = '23:00';
+    }
+
+
+    public function updatedtimeSlot($time)
+    {
+        $this->timeSlot = $time;
+        $this->state['start_time'] = $time;
     }
 
 
@@ -51,6 +74,75 @@ class CreateSchedule extends Component
         }
 
         return Employee::find($this->state['employee']);
+    }
+
+
+    public function getSelectedServiceProperty()
+    {
+        if(is_null($this->state['service'])){
+            return null;
+        }
+
+        return Service::find($this->state['service']);
+    } 
+
+
+    public function getCalendarSelectedDateObjectProperty()
+    {
+        return Carbon::createFromTimestamp($this->date);
+    }
+
+
+    public function getCalnderWeekIntervalProperty()
+    {
+        return CarbonInterval::days(1)->toPeriod(
+            $this->calendarStartDate,
+            $this->calendarStartDate->clone()->addDays(6)
+        );
+    }
+
+
+    public function incrementCalendarWeek()
+    {
+        $this->calendarStartDate->addWeek();
+    }
+
+
+    public function decrementCalendarWeek()
+    {
+        $this->calendarStartDate->subWeek();
+    }
+
+
+
+    public function getWeekIsGreaterThanCurrentProperty()
+    {
+        return $this->calendarStartDate->gt(now());
+    }
+
+
+    public function setDate($timestamp)
+    {
+        $this->date = $timestamp;
+        // dump($this->calendarSelectedDateObject->setTimeFrom($this->defaultStartTime));
+        // dd($this->calendarSelectedDateObject->setTimeFrom($this->defaultEndTime));
+
+        //dd($this->scheduleCheck);
+    }
+
+
+    public function getScheduleCheckProperty()
+    {
+        return Schedule::whereDate('date', $this->calendarSelectedDateObject)->count();
+    }
+
+
+    public function getTimeSlotsForDateProperty()
+    {
+        $start = $this->calendarSelectedDateObject->setTimeFrom($this->defaultStartTime);
+        $end = $this->calendarSelectedDateObject->clone()->setTimeFrom($this->defaultEndTime);
+
+        return CarbonInterval::minutes(self::INCREMENT)->toPeriod($start, $end);
     }
 
 
--- resources/views/livewire/create-schedule.blade.php
+++ resources/views/livewire/create-schedule.blade.php
@@ -21,6 +21,100 @@
                 @endforeach
             </select>
         </div>
+
+
+
+
+        <div class="mb-6 {{!$this->selectedService || !$this->selectedEmployee ? 'invisible' : ''}}">
+            <div class="bg-white rounded-lg">
+                
+                <div class="flex items-center justify-center relative">
+
+                    @if($this->WeekIsGreaterThanCurrent)
+                        <button type="button" class="p-4 absolute left-0 top-0" wire:click="decrementCalendarWeek">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 hover:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+                            </svg>
+                        </button>
+                    @endif
+
+
+
+                    <div class="text-lg font-semibold p-4">
+                        {{$this->calendarStartDate->format('M Y')}}
+                    </div>
+
+
+                    <button type="button" class="p-4 absolute right-0 top-0" wire:click="incrementCalendarWeek">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 hover:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                        </svg>
+                    </button>
+
+                </div>
+
+
+
+                <div class="flex justify-between items-center px-3 border-b border-gray-200 pb-2">
+
+                    @foreach($this->calnderWeekInterval as $day)
+                        
+                        <button type="button" class="text-center group cursor-pointer focus:outline-none" wire:click="setDate({{$day->timestamp}})">
+
+                            <div class="text-xs leading-none mb-2 text-gray-700">{{$day->format('D')}}</div>
+
+                            <div class="{{$date === $day->timestamp ? 'bg-gray-300' : ''}} text-lg leading-none p-1 rounded-full w-9 h-9 flex items-center justify-center group-hover:bg-gray-200">{{$day->format('d')}}</div>
+
+                        </button>
+
+                    @endforeach
+                </div>
+
+
+
+
+
+
+                <div class="max-h-52 overflow-y-auto">
+                    @if(!$this->scheduleCheck)
+                        @foreach($this->timeSlotsForDate as $slot)
+
+                            <input type="radio" name="time" id="time_{{$slot->timestamp}}" value="{{$slot->timestamp}}" wire:model="timeSlot" class="sr-only">
+
+                            <label for="time_{{$slot->timestamp}}" class="w-full text-left focus:outline-none px-4 py-2 flex items-center cursor-pointer border-b border-gray-100">
+
+                                @if($slot->timestamp == $timeSlot)
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                                    </svg>
+                                @endif
+
+                                <span>{{$slot->format('g:i A')}}</span>    
+                            </label>
+
+                        @endforeach
+                    @else
+                        <div class="text-center text-gray-700 px-4 py-2">
+                            Not available for new schedule
+                        </div>
+                    @endif
+
+                </div>
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+        
+
+
+
+
         
 
     </form>
